9758a6ef0f7e6a0471603a8801da036260f579c4
Ziwa Ali <aziwa1991@gmail.com>
2019-03-12 10:18:17 +0300

    Adds travis

---
+++ MeetupDemoPres/MeetupDemoPres.h
@@ -0,0 +1,19 @@
+//
+//  MeetupDemoPres.h
+//  MeetupDemoPres
+//
+//  Created by ali ziwa on 12/03/2019.
+//  Copyright © 2019 ali ziwa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MeetupDemoPres.
+FOUNDATION_EXPORT double MeetupDemoPresVersionNumber;
+
+//! Project version string for MeetupDemoPres.
+FOUNDATION_EXPORT const unsigned char MeetupDemoPresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MeetupDemoPres/PublicHeader.h>
+
+
